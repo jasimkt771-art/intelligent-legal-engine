e7b2dff701612aabac54591aa732044a17acda74
Jasim KT <jasimkt771@gmail.com>
2026-08-31 21:32:00 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import streamlit as st
 import uuid
 from retrieval import get_context
 from memory import (get_memory, fetch_history, save_turn)
-from cache import (connect_to_redis, connect_to_cache_index, check_cache, save_to_cache, show_exact_cache, clear_cache)
+from cache import (connect_to_cache, check_cache, save_to_cache, show_exact_cache, clear_cache)
 from llm import generate_response
 
 def initialize_connections():
-    redis_client = connect_to_redis()
-    cache_index = connect_to_cache_index()
-
+    redis_client, cache_index = connect_to_cache()
     return redis_client, cache_index
 
 def load_chat_history(session_id):
@@ -27,6 +25,9 @@ def process_query(query, session_id, redis_client, cache_index):
     response = check_cache(redis_client, cache_index, query)
 
     if response:
+        print(f'\nCached\n'
+              f'Query:\n{query}\n\n'
+              f'WITH\n\nResponse:\n{response}\n\n')
         return response
 
     contexts = get_context(query)
@@ -34,11 +35,13 @@ def process_query(query, session_id, redis_client, cache_index):
     history = get_memory(session_id)
 
     response = generate_response(query, contexts, history)
+    print("\nResponse is from LLLM\n")
 
     save_turn(session_id, query, response)
 
+    print(f'Saved\nQuery:\n{query}\n\nAND\n\nResponse:\n{response}\n\nTo database chat_history with session_id: {session_id}\n\n')
     save_to_cache(redis_client, cache_index, query, response)
-
+    print()
     return response
 
 def main():
@@ -52,20 +55,27 @@ def main():
 
     redis_client, cache_index = initialize_connections()
 
+    if "session_id" not in st.session_state:
+        st.session_state.session_id = str(uuid.uuid4())
+        print('\n=======New Conversation======')
+        print("New Session id: ", st.session_state.session_id)
+
+    else:
+        session_id = st.session_state.session_id
+        print('\n=======New Sub-Conversation======')
+        print("Current Session id: ", session_id)
+
     with st.sidebar:
         col1, col2 = st.columns([4, 1])
 
         with col1:
             if st.button("+ New Chat"):
                 st.session_state.session_id = str(uuid.uuid4())
+                print('\n=======New Conversation(Button)======')
+                print("New Session id: ", st.session_state.session_id)
 
         with col2:
             st.button("⚙️")
-
-        if "session_id" not in st.session_state:
-            st.session_state.session_id = str(uuid.uuid4())
-
-        session_id = st.session_state.session_id
 
         st.divider()
 
